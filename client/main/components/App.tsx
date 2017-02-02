ad789e4eefb9c0d8e9f7a9f5c6f6a06f5830aea3
Artem Isaenok <artem.isaenok@accenture.com>
2017-02-02 15:45:45 +0300

    Added dispatching an action getHints.

--- client/main/components/App.tsx
+++ client/main/components/App.tsx
@@ -2,17 +2,24 @@ import { Dispatch } from 'redux'; //it calls action to update state of app
 import { connect } from 'react-redux';
 import * as React from 'react';
 
-import { Header } from '../../typeahead';
+import { 
+	Header,
+	model,
+	getHints 
+} from '../../typeahead';
 
 interface AppProps {
-
+	hints: model.ProfileCard[],
+	dispatch: Dispatch<{}>;
 }
 
 class App extends React.Component<AppProps, void> {
 	render() {
+		const { dispatch } = this.props;
+		
 		return(
 			<div className="typeaheadapp">
-				<Header />
+				<Header getHints={(text: string) => dispatch(getHints(text))} />
 			</div>
 		);
 	}
