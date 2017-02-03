0357a7501a1da6855033b820de1a7b330b934ea1
Artem Isaenok <artem.isaenok@accenture.com>
2017-02-03 17:54:26 +0300

    Initial commit.

--- client/typeahead/actions.ts
+++ client/typeahead/actions.ts
@@ -1,12 +1,37 @@
-import { createAction, Action } from 'redux-actions';
+import { createAction, Action } from 'redux-actions'
 
-import { RequestHint } from './model';
+import { RequestHint, IState } from './model'
 
-import { GET_HINTS } from './constants/ActionTypes';
+import { Dispatch } from 'redux'
+
+import { 
+	GET_HINTS,
+	REQUEST_ITEMS,
+	RECEIVE_ITEMS,
+	FETCH_ITEMS
+} from './constants/ActionTypes'
 
 const getHints = createAction<RequestHint, string>(
 	GET_HINTS,
 	(text: string) => ({value: text})
-);
+)
+
+const requestItems = createAction<string, string>(
+	REQUEST_ITEMS,
+	(text: string) => "FETHCING START"
+)
+
+const receiveItems = createAction<IState, any>(
+	RECEIVE_ITEMS,
+	(items: IState) => (items)
+)
+
+const fetchItems = createAction<any, string>(
+	FETCH_ITEMS,
+	(text: string, dispatch: Dispatch<{}>) => {
+		dispatch(requestItems(text));
+		return dispatch(receiveItems({}));
+	}
+}
 
 export { getHints }
--- client/typeahead/constants/ActionTypes.ts
+++ client/typeahead/constants/ActionTypes.ts
@@ -1,1 +1,4 @@
 export const GET_HINTS = "GET_HINTS";
+export const REQUEST_ITEMS = "REQUEST_ITEMS";
+export const RECEIVE_ITEMS = "REQUEST_ITEMS";
+export const FETCH_ITEMS = "FETCH_ITEMS";
